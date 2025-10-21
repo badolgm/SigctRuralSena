c6f5028cd22b4a9c852bb1547d0c6b12ae39bb1d
badolgm <badolfogm@gmail.com>
2025-10-20 22:35:55 -0500

    fix(render): Eliminar sudo y ejecutar script de dependencias

--- install_system_deps.sh
+++ install_system_deps.sh
@@ -4,9 +4,9 @@
 
 echo "Instalando dependencias críticas del sistema..."
 
-# Actualizar la lista de paquetes e instalar librerías necesarias para Python (OpenCV y Pillow)
-sudo apt-get update -y
-sudo apt-get install -y \
+# Actualizar la lista de paquetes e instalar librerías SIN 'sudo'
+apt-get update -y
+apt-get install -y \
     build-essential \
     libpq-dev \
     libjpeg-dev \
