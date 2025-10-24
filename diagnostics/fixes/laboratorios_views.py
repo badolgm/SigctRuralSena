28ed552104d3a518324dd3af5daff84369185cb4
badolgm <badolfogm@gmail.com>
2025-10-24 16:44:23 -0500

    feat(nav): Implementación limpia de TopNav, BottomNav, y estilos responsive (FASE 1)

---
+++ diagnostics/fixes/laboratorios_views.py
@@ -0,0 +1,7 @@
+# Auto-generado por super_diagnostico v2.0
+# App: laboratorios | Archivo: views.py
+from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from django.db import models
+
+# TODO: Completar implementación real
